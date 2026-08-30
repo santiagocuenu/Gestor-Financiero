7ccf2acc57aebc00fc632e2424a832afa4801c45
Santiago Cuenu <cuenu.alomia@gmail.com>
2026-08-30 12:50:19 -0500

    Delete exceptions.py

--- exceptions.py
+++
@@ -1,8 +0,0 @@
-class GestorFinanzasError(Exception):
-    """Base para errores del dominio."""
-
-class TransaccionInvalidaError(GestorFinanzasError):
-    """Datos de transacción inválidos."""
-
-class PersistenciaError(GestorFinanzasError):
-    """Fallo en lectura o escritura de datos."""
